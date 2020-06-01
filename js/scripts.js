acddaeb763c32eb25c2b66a460cbabfe8d0d0ce5
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-01 15:59:01 -0700

    add the reverse string function to return our first and last letters of the string both capitalized and reversed
    Co-Authored-By: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 let capital = function(sentence) {
   return ((sentence.charAt(0)+ sentence.charAt(sentence.length-1)).toUpperCase());
 }
+
+let reverseString = function(str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
+}
+
 let sentence = prompt("Enter a sentence:");
 
-console.log(capital(sentence));
+console.log(reverseString(capital(sentence)));
 
